2d4a9e25fa7eb06082b101d17ab99701085bac07
Daniel Maslowski <info@orangecms.org>
2023-10-23 22:06:21 +0200

    examples/serial: tidy up, print RISC-V banner and info

--- examples/serial.rs
+++ examples/serial.rs
@@ -17,6 +17,53 @@ use hal::uart::UartTx;
 use hal::{pac, peripherals, Peripherals};
 use {ch58x_hal as hal, panic_halt as _};
 
+const RISCV_BANNER: &str = r"
+ ____   ___  ____    ____     __     __
+|  _ \ |_ _|/ ___|  / ___|    \ \   / /
+| |_) | | | \___ \ | |    _____\ \ / /
+|  _ <  | |  ___) || |___|_____|\ V /
+|_| \_\|___||____/  \____|       \_/
+";
+
+/* see https://five-embeddev.com/riscv-isa-manual/latest/machine.html */
+fn machine_info<T: ch58x_hal::uart::BasicInstance>(serial: &mut UartTx<T>) {
+    match riscv::register::misa::read() {
+        None => {
+            writeln!(serial, "ISA unknown").unwrap();
+        }
+        Some(v) => {
+            writeln!(serial, "ISA: {v:x?}").unwrap();
+        }
+    }
+
+    match riscv::register::mvendorid::read() {
+        None => {
+            writeln!(serial, "vendor unknown").unwrap();
+        }
+        Some(v) => {
+            writeln!(serial, "vendor: {v:x?}").unwrap();
+        }
+    }
+
+    match riscv::register::marchid::read() {
+        None => {
+            writeln!(serial, "arch. ID unknown").unwrap();
+        }
+        Some(v) => {
+            writeln!(serial, "arch. ID: {v:x?}").unwrap();
+        }
+    }
+
+    match riscv::register::mimpid::read() {
+        None => {
+            writeln!(serial, "impl. ID unknown").unwrap();
+        }
+        Some(v) => {
+            writeln!(serial, "impl. ID: {v:x?}").unwrap();
+        }
+    }
+}
+
 #[ch32v_rt::entry]
 fn main() -> ! {
     // hal::sysctl::Config::pll_60mhz().freeze();
@@ -34,55 +81,47 @@ fn main() -> ! {
     let mut serial = UartTx::new(p.UART1, p.PA9, Default::default()).unwrap();
     //let mut serial = UartTx::new(p.UART3, p.PA5, Default::default()).unwrap();
     //let mut serial = UartTx::new(p.UART0, p.PA14, Default::default()).unwrap();
-
     //let mut serial = UartTx::new(p.UART0, p.PB7, Default::default()).unwrap();
 
-    let mut download_button = Input::new(p.PB22, Pull::Up);
-    let mut reset_button = Input::new(p.PB23, Pull::Up);
+    let download_button = Input::new(p.PB22, Pull::Up);
+    let reset_button = Input::new(p.PB23, Pull::Up);
+
     let mut rtc = Rtc {};
+    rtc.set_datatime(DateTime {
+        year: 2023,
+        month: 10,
+        day: 16,
+        hour: 15,
+        minute: 42,
+        second: 10,
+    });
 
-    serial.blocking_flush();
-    //      rtc.set_datatime(DateTime {
-    //        year: 2023,
-    //        month: 10,
-    //        day: 16,
-    //        hour: 15,
-    //        minute: 42,
-    //        second: 10,
-    //    });
-
-    writeln!(serial, "\n\n\nHello World!").unwrap();
+    let _ = serial.blocking_flush();
+    writeln!(serial, "\n\nHello WCH! 🦀").unwrap();
     writeln!(serial, "Clocks: {}", hal::sysctl::clocks().hclk).unwrap();
-    writeln!(serial, "ChipID: {:02x}", hal::signature::get_chip_id());
+    writeln!(serial, "ChipID: {:02x}", hal::signature::get_chip_id()).unwrap();
     let now = rtc.now();
-    writeln!(serial, "Boot time: {} weekday={}", now, now.isoweekday()).unwrap();
+    writeln!(serial, "Boot time: {now} weekday={}", now.isoweekday()).unwrap();
 
-    let marchid = riscv::register::marchid::read().unwrap();
-    writeln!(serial, "marchid: 0x{:08x?}", marchid.bits());
-    let mias = riscv::register::misa::read().unwrap();
-    writeln!(serial, "mias: 0x{:08x?}", mias.bits());
+    writeln!(serial, "{RISCV_BANNER}");
+    machine_info(&mut serial);
+
+    let now = rtc.now();
+    writeln!(serial, "Time: {now} weekday={}", now.isoweekday()).unwrap();
+
+    writeln!(serial, "day {:?}", rtc.counter_day()).unwrap();
+    writeln!(serial, "2s {:?}", rtc.counter_2s()).unwrap();
 
     loop {
-        blue_led.toggle();
-
-        // writeln!(uart, "day {:?}", rtc.counter_day()).unwrap();
-        // writeln!(uart, "2s {:?}", rtc.counter_2s()).unwrap();
-
-        //  writeln!(uart, "tick! {}", SysTick::now()).unwrap();
-        delay.delay_ms(1000);
-
-        let now = rtc.now();
-        writeln!(
-            serial,
-            "{}:  button: download={} reset={}",
-            now,
-            // now.isoweekday(),
-            download_button.is_low(),
-            reset_button.is_low()
-        )
-        .unwrap();
         // serial.blocking_flush();
-        //writeln!(serial, "Current time: {} weekday={}", now, now.isoweekday()).unwrap();
-        //writeln!(serial, "button: {} {}", ).unwrap();
+        if download_button.is_low() {
+            blue_led.set_low();
+        } else {
+            blue_led.set_high();
+        }
+
+        // FIXME: systick counter is not increasing, delay has no effect
+        // writeln!(serial, "tick! {}", SysTick::now()).unwrap();
+        delay.delay_ms(1000);
     }
 }
